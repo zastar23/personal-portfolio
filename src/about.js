140ba153ab52298d89b492d5bcfc10b72deb36ce
zastar23 <florin.porut@gmail.com>
2022-10-23 16:30:27 +0300

    Errors fixed

--- src/about.js
+++ src/about.js
@@ -1,46 +1,45 @@
-import './nav.js'
-import './clock.js'
+import "./nav.js";
 
 // about text and scroll component
-const aboutContainer = document.querySelector('.about')
-const squares = document.querySelectorAll('.square-mark')
-const circles = document.querySelectorAll('.red-circle')
-const line = document.querySelector('.about-content-scroll-component > hr')
-const paragraphs = document.querySelectorAll('.about-content-text > p')
+const aboutContainer = document.querySelector(".about");
+const squares = document.querySelectorAll(".square-mark");
+const circles = document.querySelectorAll(".red-circle");
+const line = document.querySelector(".about-content-scroll-component > hr");
+const paragraphs = document.querySelectorAll(".about-content-text > p");
 
-aboutContainer.addEventListener('touchend', handleToggle)
+aboutContainer.addEventListener("touchend", handleToggle);
 
-window.addEventListener('resize', () => {
+window.addEventListener("resize", () => {
 	if (window.innerWidth >= 760) {
-		aboutContainer.removeEventListener('touchend', handleToggle)
+		aboutContainer.removeEventListener("touchend", handleToggle);
 	}
 	if (window.innerWidth <= 760) {
-		aboutContainer.addEventListener('touchend', handleToggle)
+		aboutContainer.addEventListener("touchend", handleToggle);
 	}
-})
+});
 
 const toggleStyle = function (element) {
 	element.forEach((el) => {
-		el.classList.toggle('active')
-		el.classList.toggle('inactive')
-	})
-}
+		el.classList.toggle("active");
+		el.classList.toggle("inactive");
+	});
+};
 
 function handleToggle(e) {
 	if (
-		e.target.classList.contains('active') ||
-		e.target.classList.contains('inactive') ||
-		e.target.classList.contains('active-text') ||
-		e.target.classList.contains('inactive-text')
+		e.target.classList.contains("active") ||
+		e.target.classList.contains("inactive") ||
+		e.target.classList.contains("active-text") ||
+		e.target.classList.contains("inactive-text")
 	) {
-		toggleStyle(squares)
-		toggleStyle(circles)
-		line.classList.toggle('active')
-		line.classList.toggle('inactive')
+		toggleStyle(squares);
+		toggleStyle(circles);
+		line.classList.toggle("active");
+		line.classList.toggle("inactive");
 
 		paragraphs.forEach((el) => {
-			el.classList.toggle('active-text')
-			el.classList.toggle('inactive-text')
-		})
+			el.classList.toggle("active-text");
+			el.classList.toggle("inactive-text");
+		});
 	}
 }
