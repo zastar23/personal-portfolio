65aa0d3a52b82f396c2268bba6c803e1ed4852b6
zastar23 <florin.porut@gmail.com>
2022-10-27 22:55:56 +0300

    Mobile nav fixed, text rephrased and more

--- src/nav.js
+++ src/nav.js
@@ -1,16 +1,25 @@
-let burger = document.getElementById('burger'),
-	nav = document.getElementById('main-nav')
+let burger = document.getElementById("burger"),
+	nav = document.getElementById("main-nav");
 
-burger.addEventListener('click', function (e) {
-	this.classList.toggle('is-open')
-	nav.classList.toggle('is-open')
-})
+const links = document.querySelectorAll(".link");
 
-window.addEventListener('resize', () => {
+burger.addEventListener("click", function (e) {
+	this.classList.toggle("is-open");
+	nav.classList.toggle("is-open");
+});
+
+window.addEventListener("resize", () => {
 	if (window.innerWidth <= 760) {
-		burger.classList.remove('is-open')
-		nav.classList.remove('is-open')
+		burger.classList.remove("is-open");
+		nav.classList.remove("is-open");
 	}
-})
+});
 
-export * from './nav.js'
+nav.addEventListener("click", (e) => {
+	if (!e.target.classList.contains("link")) return;
+
+	nav.classList.remove("is-open");
+	burger.classList.remove("is-open");
+});
+
+export * from "./nav.js";
